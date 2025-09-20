97ccb4eb48f9fc8f79922b40f07028fc979328da
Chirag Jain <chiragj2@gmail.com>
2025-09-20 14:58:19 +0000

    Final

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import os
 
 def extract_data():
-    url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2023-01.csv.gz"
-    df = pd.read_csv(url, nrows=10000)  # sample
-    df.to_csv("/tmp/nyc_taxi_raw.csv", index=False)
-    print("✅ Extracted data")
+    url = "https://data.cityofnewyork.us/api/views/4b4i-vvec/rows.csv?accessType=DOWNLOAD"
+    df = pd.read_csv(url, nrows=10000)
+    os.makedirs("/tmp/data", exist_ok=True)
+    raw_path = "/tmp/data/nyc_taxi_raw.csv"
+    df.to_csv(raw_path, index=False)
+    print(f"✅ Extracted data to {raw_path}")
+    return raw_path
